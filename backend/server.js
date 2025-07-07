cc5295d19b6eb2ca4d905134452e53e283d621f5
Fahad <vip.606@outlook.com>
2025-07-08 00:32:49 +0300

    update cors origin to include vercel link

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://college-login-system.vercel.app/",
+      "https://college-login-system.vercel.app",
     ],
     credentials: true,
   })
